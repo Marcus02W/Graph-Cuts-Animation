e5764770cc47a0c8df250143d14fed132eb2d005
Marcus02W <marcuswirth.02@gmail.com>
2024-01-28 18:13:27 +0100

    descriptions added to video

--- graph_theory_basics.py
+++ graph_theory_basics.py
@@ -25,12 +25,12 @@ class GraphTheoryScene(Scene):
         # Creating arrow for vertex
         arrow_1 = Line(
             start=g.vertices[2].get_center()+(1.1, 2.2, 0),
-            end=g.vertices[2].get_center()+(0.1, 0.2, 0),
+            end=g.vertices[2].get_center()+(0.3, 0.8, 0),
             color=GOLD,
             stroke_width=10
         )
 
-        text_vertices = Tex("Knoten", color=GOLD)
+        text_vertices = Tex("Node", color=GOLD)
         text_vertices.next_to(arrow_1.start, RIGHT, buff=0.2)
 
         self.play(Create(arrow_1))
@@ -40,7 +40,7 @@ class GraphTheoryScene(Scene):
         
 
         # Waiting before adding edges
-        self.wait(4)
+        #self.wait(1)
 
         # Adding edges to the graph
         edge_lines = [Line(g.vertices[e[0]].get_center(), g.vertices[e[1]].get_center(), color=WHITE) for e in edges]
@@ -50,12 +50,12 @@ class GraphTheoryScene(Scene):
         # Creating arrow for edge
         arrow_2 = Line(
             start=g.vertices[2].get_center()+(g.vertices[1].get_center()-g.vertices[2].get_center())/2+(2,-2,0),
-            end=g.vertices[2].get_center()+(g.vertices[1].get_center()-g.vertices[2].get_center())/2,
+            end=g.vertices[2].get_center()+(g.vertices[1].get_center()-g.vertices[2].get_center())/2+(0.2, -0.3, 0),
             color=GOLD,
             stroke_width=10
         )
 
-        text_edges = Tex("Kante", color=GOLD)
+        text_edges = Tex("Edge", color=GOLD)
         text_edges.next_to(arrow_2.start, RIGHT, buff=0.2)
 
         text_weight = Tex("0.75", color=WHITE)
@@ -67,7 +67,7 @@ class GraphTheoryScene(Scene):
         self.play(Write(text_weight))
 
         # removing graph from the scene
-        self.wait(2)
+        self.wait(4)
         self.play(FadeOut(arrow_1), FadeOut(text_vertices))
         self.play(FadeOut(arrow_2), FadeOut(text_edges))
         self.play(FadeOut(g), FadeOut(g_edges), FadeOut(text_weight))
@@ -115,22 +115,26 @@ class GraphTheoryScene(Scene):
             edge_config={(e[0], e[1]): {"stroke_color": WHITE} for e in edges}
         )
 
+        edges_rectangle = [(1, 2), (1, 3), (1, 4), (1, 6), (2, 3),
+                 (2, 4), (2, 5), (3, 4), (3, 5),
+                 (3, 6), (3, 8), (4, 5), (4, 6), (4, 7), (5, 6), (5, 8), (5, 7), (6, 7), (6, 8), (7,8)]
+        
         g4 = Graph(
             vertices=vertices,
-            edges=edges,
+            edges=edges_rectangle,
             layout=graph_pos_layout, layout_scale=3, labels=False,
             vertex_config={v: {"fill_color": BLUE, "radius": 0.3} for v in vertices},
             edge_config={(e[0], e[1]): {"stroke_color": WHITE} for e in edges}
         )
 
         self.play(ScaleInPlace(g1, 0.6))
-        self.play(g1.animate.shift((-3.5, 2.25, 0)))
+        self.play(g1.animate.move_to((-3.8, 2.25, 0)))
         self.play(ScaleInPlace(g2, 0.6))
-        self.play(g2.animate.shift((3.5, 1.8, 0)))
+        self.play(g2.animate.move_to((3.6, 1.4, 0)))
         self.play(ScaleInPlace(g3, 0.6))
-        self.play(g3.animate.shift((-3.5, -2.25, 0)))
+        self.play(g3.animate.move_to((-4.2, -2, 0)))
         self.play(ScaleInPlace(g4, 0.6))
-        self.play(g4.animate.shift((3.5, -2.25, 0)))
+        self.play(g4.animate.move_to((3.5, -2.25, 0)))
 
         self.wait(5)
 
@@ -150,9 +154,7 @@ class GraphTheoryScene(Scene):
 
         # Define the graph
         vertices = [1, 2, 3, 4, 5, 6, 7, 8]
-        edges = [(1, 2), (1, 3), (1, 4), (1, 6), (2, 3),
-                 (2, 4), (2, 5), (3, 4), (3, 5),
-                 (3, 6), (3, 8), (4, 5), (4, 6), (4, 7), (5, 6), (5, 8), (5, 7), (6, 7), (6, 8), (7,8)]
+        
         graph_pos_layout = {    
             1: (-3, -1, 0),
             2: (-3, 1, 0),
@@ -166,7 +168,7 @@ class GraphTheoryScene(Scene):
         # Creating the graph
         g = Graph(
             vertices=vertices,
-            edges=edges,
+            edges=edges_rectangle,
             layout=graph_pos_layout, layout_scale=3, labels=False,
             vertex_config={v: {"fill_color": BLUE, "radius": 0.3} for v in vertices},
             edge_config={(e[0], e[1]): {"stroke_color": WHITE} for e in edges}
@@ -180,20 +182,57 @@ class GraphTheoryScene(Scene):
         vertices_to_change = [6, 7, 8]
         edges_to_change = [(6, 7), (6, 8), (7, 8)]
 
+        text_subgraph = Tex("Subgraph", color=WHITE)
+        text_subgraph.move_to((0, 2.5, 0))
+        self.play(Write(text_subgraph))
+        self.play(g.animate.move_to((0, -1, 0)))
+        self.wait(3)
+        text_subgraph2 = Text("G(V^, E^) is subgraph of G(V, E) if: \n\n     - V^ ⊆ V \n     - E^ ⊆ E \n     - for each e in E^ its nodes are in V^", color=WHITE, should_center=True).scale(0.4)
+        text_subgraph2.move_to((0, 1.5, 0))
+        self.play(Write(text_subgraph2))
+        self.wait(3)
+
         self.play(
             *[g.vertices[v].animate.set_fill_color(ORANGE) for v in vertices_to_change],
             *[ApplyMethod(g.edges[e].set_color, ORANGE) for e in edges_to_change],
             run_time=4
         )
 
-        self.wait(10)
+        self.wait(3)
+        self.play(FadeOut(text_subgraph, text_subgraph2))
+
+        # text about connectivity
+        text_connectivity = Tex("Connected nodes", color=WHITE)
+        text_connectivity.move_to((0, 2.5, 0))
+        self.play(Write(text_connectivity))
+        self.wait(1)
+        text_connectivity2 = Text("In undirected graphs: Two nodes are connected, if there is a path from one node to the other \n\n => all nodes are connected to one another in our graph => connected graph", color=WHITE, should_center=True).scale(0.4)
+        text_connectivity2.move_to((0, 1.5, 0))
+        self.play(Write(text_connectivity2))
+        self.wait(3)
+        self.play(FadeOut(text_connectivity, text_connectivity2))
+
+
+        
+
+        self.wait(5)
 
 
 
 
         # === animation 4: Components in connected and in particular in disconnected graphs === #
         comp_vertices = list(set(vertices) - set(vertices_to_change))
-        comp_edges = list(set(edges) - set(edges_to_change))
+        comp_edges = list(set(edges_rectangle) - set(edges_to_change))
+
+        # text about components
+        text_components = Tex("Components in connected graphs", color=WHITE)
+        text_components.move_to((0, 2.5, 0))
+        self.play(Write(text_components))
+        self.wait(1)
+        text_components2 = Text("   - All nodes v ∈ V, which are connected to some v^ ∈ V^ \n   - All edges in E whose nodes are connected to some node v^ ∈ V^", color=WHITE).scale(0.4)
+        text_components2.move_to((0, 1.5, 0))
+        self.play(Write(text_components2))
+        self.wait(3)
 
         self.play(
             *[g.vertices[v].animate.set_fill_color(GREEN) for v in comp_vertices],
@@ -210,7 +249,7 @@ class GraphTheoryScene(Scene):
         )
 
         self.wait(7)
-
+        self.play(FadeOut(text_components, text_components2))
         self.play(FadeOut(g))
 
 
@@ -238,6 +277,18 @@ class GraphTheoryScene(Scene):
             vertex_config={v: {"fill_color": BLUE, "radius": 0.3} for v in vertices},
             edge_config={(e[0], e[1]): {"stroke_color": WHITE} for e in edges}
         )
+
+        g_2_comps.move_to((0, -1, 0))
+
+        # text about components
+        text_components = Tex("Components in disconnected graphs", color=WHITE)
+        text_components.move_to((0, 2.5, 0))
+        self.play(Write(text_components))
+        self.wait(1)
+        text_components2 = Text("   - All nodes v ∈ V, which are connected to some v^ ∈ V^ \n   - All edges in E whose nodes are connected to some node v^ ∈ V^", color=WHITE).scale(0.4)
+        text_components2.move_to((0, 1.5, 0))
+        self.play(Write(text_components2))
+        self.wait(1)
 
         self.play(Create(g_2_comps), run_time = 2)
         self.wait(2)
@@ -284,9 +335,9 @@ class GraphTheoryScene(Scene):
             run_time=4
         )
 
-        self.wait(7)
-
-        self.play(FadeOut(g))
+        self.wait(5)
+        self.play(FadeOut(text_components, text_components2))
+        self.play(FadeOut(g_2_comps))
 
 
 
